88442039f868a6635988d24d39cf44913c59922a
Aditya <adityarajbju7@gmail.com>
2026-08-05 22:50:25 +0530

    Add escape character examples and print statements for demonstration

--- 06_comments_prints_esc/02_escape.py
+++ 06_comments_prints_esc/02_escape.py
@@ -1,1 +1,3 @@
-print("Hey how are you?")
+print("Hey how are you?\n I am \\newline good")#\n used to print in new line and \\ is used to print \
+
+print("Hello \" World") #backslash is used to escape the double quotes
---
+++ 06_comments_prints_esc/03_print.py
@@ -0,0 +1,12 @@
+
+# print('Hello " World!')                      #we can use single quotes to print double quotes without using backslash
+
+# print('Hello World', "Aditya")               #we can use single quotes and double quotes together to print without using backslash
+
+
+# print("Hello World!", "Aditya" , 5 , sep = ",") #sep is used to separate the values with the given string
+
+# print("Hello World!", "Aditya" , 5 , sep = "/") #sep is used to separate the values with the given string\
+print( 'Hello World' , end = ",")
+
+print("Aditya", end = "//")
