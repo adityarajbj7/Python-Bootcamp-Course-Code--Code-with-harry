d53d58a8cc1e0a6e5e98369af45de421f7d1c232
Aditya <adityarajbju7@gmail.com>
2026-08-05 22:22:06 +0530

    Add user input examples and comments for better understanding

---
+++ 06_comments_prints_esc/01_comments.py
@@ -0,0 +1,13 @@
+print("Hey how are you?")
+
+
+#Arjun Please finish this code 
+
+# Rohan please review , what arjun has done.
+
+'''
+Taking User Input in Python
+Using the  input()  Function
+The  input()  function allows you to take user input from the keyboard.
+By default,  input()  returns a string. You can convert it to other data types as
+''' 
