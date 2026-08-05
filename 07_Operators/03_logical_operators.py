031bea5fcf16904be0b72ad166ed80e9d0d6306a
Aditya <adityarajbju7@gmail.com>
2026-08-05 23:47:38 +0530

    Add arithmetic, conditional, logical, and assignment operator examples

---
+++ 07_Operators/03_logical_operators.py
@@ -0,0 +1,20 @@
+c = True 
+d = False
+
+# Logical Operators
+#For AND operator, if both the values are True then it will return True otherwise it will return False
+print("AND Operator")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+# For OR operator, if any one of the value is True then it will return True otherwise it will return False
+print("OR Operator")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+#For NOT operator, it will return the opposite of the value. If the value is True then it will return False and if the value is False then it will return True
+print("Not Operator")
+print(not(True) ) 
+print(not(False) ) 
