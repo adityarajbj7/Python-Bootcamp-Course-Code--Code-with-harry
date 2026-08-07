790e4c56b87f860844d30618653019650aaf15dd
Aditya <adityarajbju7@gmail.com>
2026-08-07 15:55:23 +0530

    Refactor loop examples and remove redundant print statements

--- 09_loops/01_for_loops.py
+++ 09_loops/01_for_loops.py
@@ -1,4 +1,16 @@
-
-
-for i in range (1,101):
+#I will need 5 print statement to print numbers 1 to 5 , i will not do it because it is to much time taking , i can use loops in python.
+'''
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+'''
+'''
+for i in range(1, 6):     #range function goes 1 to 5 in the case
     print(i)
+'''
+
+#This is a table maker for loops
+for i in range(1, 11):
+    print("5 X", i, "=", 5*i)
