c8d7b5048f03abd3904684ab27324927813ab420
Aditya <adityarajbju7@gmail.com>
2026-08-13 13:20:44 +0530

    Update slicing example and desktop.ini icons
    Simplify string slicing example: set name to "Harry", remove the commented block and enable prints (print(name[0:2]) and print(name[2:-1])). Update desktop.ini files: change IconResource index in 02_Python_Syntax to SHELL32.dll,4 and update the root desktop.ini icon path to Documents\Programming icons\python.ico. Add a new empty file Rough Practice/New.py.

--- 10_Strings/03_slicing.py
+++ 10_Strings/03_slicing.py
@@ -1,7 +1,6 @@
-name = "Harry123456789"
+name = "Harry"
 
-'''
-print(name[0:2]) # goes from 0 to 2-1 ie 0 to 1
 
-print(name[2:-1]) # Same as name[2:4]
-'''
+print(name[0:2]) #goes from 0 to 2-1 i.e. 0 to 1
+
+print(name[2:-1]) #Same as name[2:4]
