1d109d16d19967398c9188e94933a81c897a9d2d
Aditya <adityarajbju7@gmail.com>
2026-08-25 21:58:52 +0530

    Add initial implementation of average function with example usage

---
+++ 11_functions/01_functions.py
@@ -0,0 +1,25 @@
+# a = 2
+# b = 4
+# c = 6
+
+# average = (a+b+c)/3
+# print(average)
+
+# a1 = 9245892
+# b1 = 78
+# c1 = 690
+
+# average1 = (a1+b1+c1)/3
+# print(average1)
+
+
+def average(a,b,c):
+    d = (a + b + c)/3.0
+    # print(d)
+    return d
+
+o1 = average(3 , 5 , 1)
+o2 = average(8 , 8 , 57)
+
+print(o1)
+print(o2)
