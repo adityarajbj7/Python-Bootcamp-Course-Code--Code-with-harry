a0d808524f79b6d04c0fb96db36de1e239d19248
Aditya <adityarajbju7@gmail.com>
2026-08-29 16:03:08 +0530

    Add lambda function implementations for square and sum with example usage

---
+++ 11_functions/03_lambda.py
@@ -0,0 +1,15 @@
+square = lambda x: x*x
+'''
+As Good as writing 
+def square(x)
+    return x*x
+''' 
+
+sum = lambda x,y: x+y
+'''
+As good as writing
+def sum(x,y):
+    return x + y
+'''
+print(square(78))
+print(sum(7,3))
