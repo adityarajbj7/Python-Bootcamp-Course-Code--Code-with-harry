282a2918f2d68a21bc7e2a2fa758de4a3b785589
Aditya <adityarajbju7@gmail.com>
2026-09-01 06:54:31 +0530

    Add example implementation of tuples with explanations

---
+++ 13_tuples/01_tuples.py
@@ -0,0 +1,10 @@
+a = (3, 2, 22, 13)
+
+print(a)  # Output: (3, 2, 22, 13)
+print(a[2])  # Output: 22
+print(a[3])  # Output: 13
+# a[3] = 15  # This will raise a TypeError because tuples are immutable
+
+b = (2, ) # This is a tuple with a single element that requires a comma after the element to differentiate it from a regular parenthesis expression.
+
+
