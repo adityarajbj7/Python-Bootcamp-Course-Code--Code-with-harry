341e272239feea662ff7e8260c327e8111dcbc7b
Aditya <adityarajbju7@gmail.com>
2026-09-01 06:54:45 +0530

    Add implementation of list comprehension to generate table of 5

---
+++ 12_lists/03_list_comprehension.py
@@ -0,0 +1,15 @@
+ # Create a list containing table of 5
+
+# a = 5
+# table = []
+
+# for i in range(1, 11):
+#     table.append(5*i)
+# print(table)
+
+'''
+ Using list comprehension to create the same table of 5
+'''
+
+table = [5*i for i in range(1, 11)]
+print(table)
