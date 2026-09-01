1de27a320649877ecac8202a09bf564315ca2f89
Aditya <adityarajbju7@gmail.com>
2026-09-01 06:58:29 +0530

    Add implementation of tuple unpacking with example usage

---
+++ 13_tuples/02_tuple_unpacking.py
@@ -0,0 +1,8 @@
+tu = (3, 2, 45)
+
+a, b, c = tu  # This is tuple unpacking where each variable gets the corresponding value from the tuple.
+print(a)  # Output: 3
+print(b)  # Output: 2
+print(c)  # Output: 45
+
+print(a, b, c)  # Output: 3 2 45
